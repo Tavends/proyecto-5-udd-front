c994a6e6ca31497de4e07a2d467c125f68fb57e5
Tomas Avendaño Soriano <toavendano.dwfsc5@bootcampudd.cl>
2023-07-17 17:21:57 -0400

    Footer

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -6,45 +6,8 @@ import UserContext from '../../contexts/user/UserContext.jsx';
 export const Products = () => {
   const userCtx = useContext( UserContext )
   const { user } = userCtx
-  //llama a la api para ver productos (/products)
-  const [ products, setProducts ] = useState( [] )
-  // const products = [ {
-  //   id: "1",
-  //   title: "Producto 1",
-  //   description: "Descripcion",
-  //   price: 50444,
-  //   imageUrl: "https://d1ih8jugeo2m5m.cloudfront.net/2022/12/productos-mas-vendidos-del-mundo.jpg"
-  // }, {
-  //   id: "2",
-  //   title: "Producto 1",
-  //   description: "Descripcion",
-  //   price: 50444,
-  //   imageUrl: "https://d1ih8jugeo2m5m.cloudfront.net/2022/12/productos-mas-vendidos-del-mundo.jpg"
-  // }, {
-  //   id: "3",
-  //   title: "Producto 1",
-  //   description: "Descripcion",
-  //   price: 50444,
-  //   imageUrl: "https://d1ih8jugeo2m5m.cloudfront.net/2022/12/productos-mas-vendidos-del-mundo.jpg"
-  // }, {
-  //   id: "4",
-  //   title: "Producto 1",
-  //   description: "Descripcion",
-  //   price: 50444,
-  //   imageUrl: "https://d1ih8jugeo2m5m.cloudfront.net/2022/12/productos-mas-vendidos-del-mundo.jpg"
-  // }, {
-  //   id: "5",
-  //   title: "Producto 1",
-  //   description: "Descripcion",
-  //   price: 50444,
-  //   imageUrl: "https://d1ih8jugeo2m5m.cloudfront.net/2022/12/productos-mas-vendidos-del-mundo.jpg"
-  // }, {
-  //   id: "6",
-  //   title: "Producto 1",
-  //   description: "Descripcion",
-  //   price: 50444,
-  //   imageUrl: "https://d1ih8jugeo2m5m.cloudfront.net/2022/12/productos-mas-vendidos-del-mundo.jpg"
-  // } ]
+ const [ products, setProducts ] = useState( [] )
+ 
   useEffect( () => {
     const getProductsFromDB = async () => {
       const axiosRes = await axiosClient.get( '/products' )
