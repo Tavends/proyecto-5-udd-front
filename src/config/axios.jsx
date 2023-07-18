ab3f8526f49fb72fb2beb46148c5ee7f1145af0e
Tomas Avendaño Soriano <toavendano.dwfsc5@bootcampudd.cl>
2023-07-18 00:28:20 -0400

    cambio axios

--- src/config/axios.jsx
+++ src/config/axios.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 const backendUrlLocal = "http://localhost:4500"
 const backendUrlProd = "https://proyecto-5-udd-api.onrender.com"
 const axiosClient = axios.create( {
-  baseURL: backendUrlLocal
+  baseURL: backendUrlProd
 
 } )
 
